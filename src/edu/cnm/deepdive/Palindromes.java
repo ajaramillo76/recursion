5cfd2ceb0bbe3440c671a48c021fd0bf71ae2a1d
Your Name Comes Here <archie808@hotmail.com>
2020-02-03 10:16:45 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive;
+
+public final class Palindromes {
+
+    private Palindromes() {} // can't create subclass
+
+    private static boolean isPalindromeClean(String source) {
+      return source.length() <= 1
+          || (
+              source.charAt(0) == source.charAt(source.length() - 1)
+              && isPalindromeClean(source.substring(1, source.length() - 1))
+          );
+    }
+
+    public static boolean isPalindrome(String source) {
+      
+    }
+
+
+
+}
